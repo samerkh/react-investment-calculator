1c2a03c9a7583216a0bb6fdfb71819fdb17a1d09
Samer Alkhatib <khatib@cotede.ps>
2024-06-24 00:31:09 +0300

    Calculate result and show in table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
-
+import InputGroup from "./components/InputGroup";
+import Result from "./components/Result";
+import { calculateInvestmentResults } from "./util/investment";
 function App() {
+  const [result, setResult] = React.useState([]);
+  const handleInputChange = (input) => {
+    const res = calculateInvestmentResults(input);
+    setResult(res);
+  };
+
   return (
     <>
       <Header />
+      <InputGroup onInputChange={handleInputChange} />
+      <Result result={result} />
     </>
   );
 }
